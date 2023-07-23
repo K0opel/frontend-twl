284890f4f3354408ba76150241f839fd38bb50f3
K0opel <rafiwitaqi@gmail.com>
2023-07-23 23:36:03 +0700

    fe

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import ProductList from "./component/ProductList";
+import AddProduct from "./component/AddProduct";
+import EditProduct from "./component/EditProduct";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductList/>}/>
+        <Route path="add" element={<AddProduct/>}/>
+        <Route path="edit/:id" element={<EditProduct/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
